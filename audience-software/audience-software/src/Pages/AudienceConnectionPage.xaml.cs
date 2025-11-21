c822d2859b47ff6fa2bd749cd98819eb1b5fdf0c
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-11-21 14:41:38 +0900

    コメントアウト等追加

--- audience-software/audience-software/src/Pages/AudienceConnectionPage.xaml.cs
+++ audience-software/audience-software/src/Pages/AudienceConnectionPage.xaml.cs
@@ -3,50 +3,118 @@ using Microsoft.UI.Xaml.Controls;
 using System;
 using System.Threading.Tasks;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace audience_software.src.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AudienceConnectionPage : Page
     {
-        //上体管理のためのENUM
+        private Audience? _audienceWindow;
+
         public enum ViewState
         {
             Setting,
             Loading,
             Completed,
             Failed,
-            Running, 
+            Running,
             End
         }
+
         private ViewState _state = ViewState.Setting;
-        private Window newWindow = new Audience();
+
         public AudienceConnectionPage()
         {
             InitializeComponent();
+            Loaded += Page_Loaded;
         }
 
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            await RenderState();
+        }
+
+        private async Task SetState(ViewState newState)
+        {
+            _state = newState;
+            await RenderState();
+        }
+
+        private async Task RenderState()
+        {
+            HiddenAllUI();
+
+            switch (_state)
+            {
+                case ViewState.Setting:
+                    MainContent.Visibility = Visibility.Visible;
+                    break;
+
+                case ViewState.Loading:
+                    LoadingContent.Visibility = Visibility.Visible;
+                    await Task.Delay(2000);
+                    //以下に接続処理を書き、接続できたらconnectSuccessをtrueにして次へ進む
+                    bool connectSuccess = false;
+                    connectSuccess = true;
+
+                    await SetState(connectSuccess ? ViewState.Completed : ViewState.Failed);
+                    break;
+
+                    //接続が失敗したら戻る
+                case ViewState.Failed:
+                    MainContent.Visibility = Visibility.Visible;
+                    ExceptionText.Visibility = Visibility.Visible;
+                    break;
+
+                    //接続が確立したときの処理
+                case ViewState.Completed:
+                    CompletedContent.Visibility = Visibility.Visible;
+                    await Task.Delay(2000);
+                    OpenAudienceWindow();
+                    await SetState(ViewState.Running);
+                    break;
+
+                    //画面共有中の処理
+                case ViewState.Running:
+                    RunningContent.Visibility = Visibility.Visible;
+                    break;
+
+                    //終わったらそれ用のUI表示
+                case ViewState.End:
+                    EndContent.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+
+        private void OpenAudienceWindow()
+        {
+            if (_audienceWindow != null)
+                return; //すでに作られてる場合は再生成しない
+
+            _audienceWindow = new Audience();
+            _audienceWindow.Closed += (_, _) =>
+            {
+                _audienceWindow = null;
+                _ = SetState(ViewState.End);
+            };
+
+            _audienceWindow.Activate();
+        }
+
+        //UIのコネクトボタンの処理
+        private async void ConnectionSubmit(object sender, RoutedEventArgs e)
+        {
+            await SetState(ViewState.Loading);
+        }
+
+        //UIの戻るボタンの処理
         private void ReturnPage(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
         {
             var frame = (App.Current as App)?._mainWindow?.NavFrame;
             if (frame != null && frame.CanGoBack)
-            {
                 frame.GoBack();
-            }
-
-
         }
-        //ページ描画時の処理
-        private async void Page_Loaded(object sender,RoutedEventArgs e)
-        {
-            await UpdateView();
-            //以下に接続処理記述(Delayはハリボテ)
-        }
-        private async Task UpdateView()
+
+        //全UI非表示
+        private void HiddenAllUI()
         {
             MainContent.Visibility = Visibility.Collapsed;
             LoadingContent.Visibility = Visibility.Collapsed;
@@ -54,59 +122,6 @@ namespace audience_software.src.Pages
             CompletedContent.Visibility = Visibility.Collapsed;
             RunningContent.Visibility = Visibility.Collapsed;
             EndContent.Visibility = Visibility.Collapsed;
-
-            switch (_state) {
-                case ViewState.Setting:
-                    MainContent.Visibility = Visibility.Visible;
-                    break;
-                case ViewState.Loading:
-                    LoadingContent.Visibility = Visibility.Visible;
-                    await Task.Delay(2000);//ハリボテ
-                    /*
-                     * ここに接続処理を書く。
-                     */
-                    _state = ViewState.Completed;
-                    await UpdateView();
-                    break;
-                case ViewState.Failed:
-                    MainContent.Visibility = Visibility.Visible;
-                    ExceptionText.Visibility = Visibility.Visible;
-                    break;
-                case ViewState.Completed:
-                    
-                    CompletedContent.Visibility = Visibility.Visible;
-                    await Task.Delay(2000);
-                    //新しいウィンドウをひらきつつ？？？
-                    
-                    newWindow.Activate();
-                    
-                    _state = ViewState.Running;
-                    await UpdateView();
-                    break;
-                case ViewState.Running:
-                    RunningContent.Visibility = Visibility.Visible;
-                    break;
-                case ViewState.End:
-                    EndContent.Visibility = Visibility.Visible;
-                    break;
-
-            }
-
-            newWindow.Closed += (s, args) =>
-            {
-                _state = ViewState.End;
-                UpdateView();
-            };
-
         }
-        private async void ConnectionSubmit(object sender, RoutedEventArgs e)
-        {
-            _state = ViewState.Loading;
-            await UpdateView();
-
-            //以下に接続時の処理
-
-        }
-
     }
 }
