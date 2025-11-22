50d14907202faa811f2e2c6adeaafd4a16600632
Dietary_fiber_hiroto <2230372@ecc.ac.jp>
2025-11-22 19:00:33 +0900

    土台構築とherosection,conceptsectionの構築

---
+++ greasetrace/src/assets/ImagesRoute.ts
@@ -0,0 +1,27 @@
+import _3409784 from "./images/3409784.png";
+import cable from "./images/cable.svg";
+import fullscreen from "./images/fullscreen.svg";
+import golang_logo_icon_171073 from "./images/golang_logo_icon_171073.png";
+import GreaseTraiceIcon from "./images/GreaseTraiceIcon.png";
+import IMG_7194 from "./images/IMG_7194.jpg";
+import IMG_7187 from "./images/IMG_7187.jpg";
+import IMG_7188 from "./images/IMG_7188.jpg";
+import logo_winui from "./images/logo-winui.png";
+import panel_top from "./images/panel-top.svg";
+import react_original_logo_icon_146374 from "./images/react_original_logo_icon_146374.png";
+import shield_half from "./images/shield-half.svg";
+
+export const ImagesRoute = {
+  _3409784: _3409784.src,
+  cable: cable.src,
+  fullscreen: fullscreen.src,
+  golang_logo_icon_171073: golang_logo_icon_171073.src,
+  GreaseTraiceIcon: GreaseTraiceIcon.src,
+  IMG_7194: IMG_7194.src,
+  IMG_7187: IMG_7187.src,
+  IMG_7188: IMG_7188.src,
+  logo_winui: logo_winui.src,
+  panel_top: panel_top.src,
+  react_original_logo_icon_146374: react_original_logo_icon_146374.src,
+  shield_half: shield_half.src,
+};
